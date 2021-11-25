029d445d0045b7c99384bd5f573b6aa219cd1c16
aaronjenkins <aaron@aaronjenkins.me>
2021-11-25 12:29:31 -0500

    add db id to code

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const { Client } = require("@notionhq/client")
-const dotenv = require("dotenv")
-dotenv.config()
+const { Client } = require('@notionhq/client');
+const dotenv = require('dotenv');
+dotenv.config();
 
 exports.handler = async (event) => {
-    
     const notion = new Client({
         auth: process.env.NOTION_TOKEN,
-      })
-    let page = await notion.databases.retrieve()
+    });
+    let page = await notion.databases.retrieve({
+        database_id: process.env.DATABASEID,
+    });
     const response = {
         statusCode: 200,
         body: JSON.stringify(page),
