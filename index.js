bc5084e87763b48af2f33b6a689666f5c71f0f34
aaronjenkins <aaron@aaronjenkins.me>
2021-11-25 13:02:09 -0500

    change from retrive to query

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
     const notion = new Client({
         auth: process.env.NOTION_TOKEN,
     });
-    let page = await notion.databases.retrieve({
+    let page = await notion.databases.query({
         database_id: process.env.DATABASEID,
     });
     const response = {
