838f6545d738430a7cae684e0ecdbfb3fb4f9cdd
aaronjenkins <aaron@aaronjenkins.me>
2021-11-26 09:10:49 -0500

    select random album from list

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ exports.handler = async (event) => {
     const notion = new Client({
         auth: process.env.NOTION_TOKEN,
     });
-    let page = await notion.databases.query({
+    let rawNotionData = await notion.databases.query({
         database_id: process.env.DATABASEID,
     });
+    let albums = rawNotionData["results"]
+    let album = albums[Math.floor(Math.random() * albums.length)]
     const response = {
         statusCode: 200,
-        body: page,
+        body: album,
     };
     return response;
 };
